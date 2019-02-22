17560e453c71397b16f5711970052fe66d71d737
Tejus Kandachar <tejus.k.v@gmail.com>
2019-02-22 12:05:32 +0530

    Test 2 - Creating a new thread by extending Thread

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,42 @@
+class NewThread extends Thread {
+
+    NewThread() {
+        //Call super constructor to create a new thread
+        super("Demo Thread");
+        System.out.println("Child thread: " + this);
+    }
+
+    //New thread calls this version of run() when instantiated
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 5; i > 0; i--) {
+                System.out.println("Child thread: " + i);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Child thread interrupted");
+        }
+        System.out.println("Exiting child thread");
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
-        Thread t = Thread.currentThread();
+        NewThread nt = new NewThread();
 
-        System.out.println("Current thread: " + t);
-        t.setName("My Thread");
-        System.out.println("After name change: " + t);
+        nt.start();
 
         try {
             for (int n = 5; n > 0; n--) {
-                System.out.println(n);
+                System.out.println("Main thread: " + n);
                 Thread.sleep(1000);
             }
         } catch (InterruptedException e) {
             System.out.println("Main thread interrupted");
         }
+        System.out.println("Main thread exiting");
     }
 }
